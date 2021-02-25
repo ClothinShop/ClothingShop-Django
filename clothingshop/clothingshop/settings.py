6a06ae89755ccf37e557e4fc996a40146b37d486
Max Borzenko <zzzking999@yandex.ru>
2021-02-25 12:24:36 +0700

    trying to hide django secret key

--- clothingshop/clothingshop/settings.py
+++ clothingshop/clothingshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!b1^caeje7chju)z)%6_%$ck*@6rs^&b4)^r&e96tszzkq1waw'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
